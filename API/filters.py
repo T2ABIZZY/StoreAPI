bdd3e8f46f6157c3ce37daed317e2c7d411e0391
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2022-04-21 17:15:44 +0100

    fixing ordering filter

--- API/filters.py
+++ API/filters.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from django_filters.rest_framework import FilterSet
 from .models import Product
 
--- API/views.py
+++ API/views.py
@@ -16,7 +16,7 @@ class ProductViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['title', 'description']
-    ordering_fields = ['unit_price', 'last_update']
+    ordering_fields = ['price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
