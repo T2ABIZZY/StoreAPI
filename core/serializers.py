44ff7170442c82e984dabf51a9fbe69beb1a38c4
Thamer Sekhri <95574184+T2ABIZZY@users.noreply.github.com>
2022-06-15 01:14:31 +0100

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -5,16 +5,16 @@ from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields=['id','account_type','username','email','password','first_name','last_name','avatar']
+        fields=['id','account_type','username','email','password','first_name','last_name','avatar','phone','description','location']
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
-        fields=['id','account_type','username','email','password','first_name','last_name','avatar']
+        fields=['id','account_type','username','email','password','first_name','last_name','avatar','phone','description','location']
 
 class CurrentUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'id','first_name','password','last_name','account_type','avatar')
+        fields = ('username', 'email', 'id','first_name','password','last_name','account_type','avatar','phone','description','location')
 
 class userProfileSerializer(serializers.ModelSerializer):
     user=CurrentUserSerializer(read_only=True)
